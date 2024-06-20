211bfc04418051e4427ba12f5237f02b3340fc95
pouatchm0406 <pouatchm0406@student.faytechcc.edu>
2024-06-20 13:34:25 -0400

    Add files via upload

---
+++ P1LAB1_PouatchaMartin.py
@@ -0,0 +1,13 @@
+# pouatcha
+# 06/20/2024
+# P1LAB1
+# Introduction to python and first programe 
+
+
+first_name = input("Enter your first name : ")
+last_name = input ("Enter your last name : ")
+print("Hello,", first_name,last_name + "!","Welcome to CTI-110")
+
+print("This program calculates thr Area of a Rectangle")
+print('------------------------------------------')
+length = int(input('please Enter Rectangle Lenght: '))
